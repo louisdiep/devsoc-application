0ac4966ad7aa83f48aaa8b1454524c771420348d
Louis Diep <louisdiepp@gmail.com>
2025-01-13 14:54:56 +1100

    add modal images

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// index.js
+
+
+// Modal Control
+// Get the modal
+var images = document.querySelectorAll('.souvenir-img');
+var modals = document.querySelectorAll('.modal');
+
+var img = document.getElementById('passport');
+var textBox = document.getElementById("passport-textbox");
+
+images.forEach(function(img) {
+    img.onclick = function() {
+      var modalId = img.id + '-modal';
+      var modal = document.getElementById(modalId);
+      var textBox = modal.querySelector('.text-box');
+  
+      modal.style.display = "block";
+      textBox.style.display = "block";
+    };
+  });
+
+  var closeButtons = document.querySelectorAll('.close');
+  closeButtons.forEach(function(span) {
+    span.onclick = function() {
+      var modal = span.closest('.modal'); 
+      var textBox = modal.querySelector('.text-box');
+      modal.style.display = "none";
+      textBox.style.display = "none"; 
+    };
+  });
