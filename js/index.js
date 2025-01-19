64212d8d9edea160a91544ae6c50e0889dc0cc94
Louis Diep <louisdiepp@gmail.com>
2025-01-19 20:57:33 +1100

    add background clouds animation

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 // index.js
 
 document.addEventListener("DOMContentLoaded", () => {
-    const sky = document.getElementById("sky");
-    if (!sky) {
-        console.error("sky element not found!");
-        return; 
-    }
+    const skies = document.querySelectorAll(".sky");
+    const skyWrapper = document.getElementById("sky-wrapper");
+    const pageHeight = document.body.scrollHeight;
+    skyWrapper.style.height = `${pageHeight}px`;
 
     function getRndInteger(min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
       }
 
-    function createClouds() {
+    function createClouds(sky) {
+        sky.style.height = `${pageHeight}px`;
+
         const cloud = document.createElement("img");
-        // const randomSize = getRndInteger(600, 1000);
-        // cloud.style.width = randomSize + "px";
+
         const randomSize = getRndInteger(600, 1000);
         cloud.style.width = randomSize + "px";
+        cloud.style.opacity = "50%";
 
         const randomTransform = getRndInteger(-100, 100);
         cloud.style.transform = `translateX(${randomTransform}px)`;
@@ -25,12 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
         cloud.classList.add("cloud");
     }
 
+    const cloudWidth = 600;
+    const pageWidth = window.innerWidth;
+    const numCloudsX = Math.ceil(pageWidth / cloudWidth);
 
+    const cloudHeight = 200;
+    const numCloudsY = Math.ceil(pageHeight / cloudHeight); 
 
-    const numClouds = 50;
-    for (let i = 0; i < numClouds; i++) {
-        createClouds();
-    }
-    
+    const numClouds = numCloudsX * numCloudsY * 3;
+
+    skies.forEach(sky => {
+        for (let i = 0; i < numClouds; i++) {
+            createClouds(sky);
+        }
+    });
+
 })
 
