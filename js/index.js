272df3d20fc8a1c2c9bd6f7dd563caa962764f29
Louis Diep <louisdiepp@gmail.com>
2025-01-24 16:47:36 +1100

    add country banners; sushi

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // index.js
 
 document.addEventListener("DOMContentLoaded", () => {
+    // create the sky and clouds
     const skies = document.querySelectorAll(".sky");
     const skyWrapper = document.getElementById("sky-wrapper");
     const pageHeight = document.body.scrollHeight;
@@ -43,5 +44,45 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+
+    // create the floating country banners
+    const countryBanners = document.querySelectorAll(".country-banner");
+    const countryWrappers = document.querySelectorAll(".country-wrapper");
+    countryWrappers.forEach(countryBanner => {
+        const sectionHeight = countryBanner.parentElement.offsetHeight;
+        countryBanner.style.height = `${sectionHeight}px`;
+    });
+
+    function createCountryBanners(countryBanner) {
+        const countryIcon = document.createElement("img");
+
+        const randomSize = getRndInteger(25, 100);
+        countryIcon.style.width = randomSize + "px";
+        countryIcon.style.opacity = "50%";
+
+        const randomTransformX = getRndInteger(-1000, 1000);
+        const randomTransformY = getRndInteger(0, 0);
+
+        countryIcon.style.transform = `translateX(${randomTransformX}px) translateY(${randomTransformY}px)`;
+
+        if (countryBanner.parentElement.id == "japan") {
+            countryIcon.src = "assets/cherry-blossom-2.png";
+        } else if (countryBanner.parentElement.id == "france") {
+            countryIcon.src = "assets/croissant.png";
+        } else if (countryBanner.parentElement.id == "italy") {
+            countryIcon.src = "assets/tiramisu-emoji.webp";
+        }
+        
+        countryBanner.append(countryIcon);
+        countryIcon.classList.add("countryIcon");
+    }
+
+    const numBannerImages = 100; // this is the number of cherry blossoms/baguettes/tiramisu
+
+    countryBanners.forEach(countryBanner => {
+        for (let i = 0; i < numBannerImages; i++) {
+            createCountryBanners(countryBanner);
+        }
+    });
 })
 
