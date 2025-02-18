cca4f29ecd4fa93d45d95425afe012a7c7ce38ca
Louis Diep <louisdiepp@gmail.com>
2025-02-18 16:36:46 +1100

    fix style and add acknowledgements

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // index.js
 
+// this handles the infinite cloud and country specific backgrounds and is
+// based off https://www.youtube.com/watch?v=lIOD8NmGd0M&t=361s
+
 document.addEventListener("DOMContentLoaded", () => {
     // create the sky and clouds
     const skies = document.querySelectorAll(".sky");
@@ -7,28 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const pageHeight = document.body.scrollHeight;
     skyWrapper.style.height = `${pageHeight + 300}px`;
 
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-      }
-
-    function createClouds(sky) {
-        sky.style.height = `${pageHeight}px`;
-
-        const cloud = document.createElement("img");
-
-        const randomSize = getRndInteger(600, 1200);
-        cloud.style.width = randomSize + "px";
-        cloud.style.opacity = "50%";
-
-        const randomTransformX = getRndInteger(-500, 500);
-        const randomTransformY = getRndInteger(-500, 500);
-
-        cloud.style.transform = `translateX(${randomTransformX}px) translateY(${randomTransformY}px)`;
-        cloud.src = "assets/cloud.png";
-        sky.append(cloud);
-        cloud.classList.add("cloud");
-    }
-
+    // randomise the number of clouds
     const cloudWidth = 600;
     const pageWidth = window.innerWidth;
     const numCloudsX = Math.ceil(pageWidth / cloudWidth);
@@ -40,10 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     skies.forEach(sky => {
         for (let i = 0; i < numClouds; i++) {
-            createClouds(sky);
+            createClouds(sky, pageHeight);
         }
     });
-
 
     // create the floating country banners
     const countryBanners = document.querySelectorAll(".country-banner");
@@ -52,30 +33,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const sectionHeight = countryBanner.parentElement.offsetHeight;
         countryBanner.style.height = `${sectionHeight}px`;
     });
-
-    function createCountryBanners(countryBanner) {
-        const countryIcon = document.createElement("img");
-
-        const randomSize = getRndInteger(25, 100);
-        countryIcon.style.width = randomSize + "px";
-        countryIcon.style.opacity = "50%";
-
-        const randomTransformX = getRndInteger(-1000, 1000);
-        const randomTransformY = getRndInteger(0, 0);
-
-        countryIcon.style.transform = `translateX(${randomTransformX}px) translateY(${randomTransformY}px)`;
-
-        if (countryBanner.parentElement.id == "japan") {
-            countryIcon.src = "assets/cherry-blossom-2.png";
-        } else if (countryBanner.parentElement.id == "france") {
-            countryIcon.src = "assets/croissant.png";
-        } else if (countryBanner.parentElement.id == "italy") {
-            countryIcon.src = "assets/tiramisu-emoji.webp";
-        }
-        
-        countryBanner.append(countryIcon);
-        countryIcon.classList.add("countryIcon");
-    }
 
     const numBannerImages = 100; // this is the number of cherry blossoms/baguettes/tiramisu
 
@@ -86,3 +43,48 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 })
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function createClouds(sky, pageHeight) {
+    sky.style.height = `${pageHeight}px`;
+
+    const cloud = document.createElement("img");
+
+    const randomSize = getRndInteger(600, 1200);
+    cloud.style.width = randomSize + "px";
+    cloud.style.opacity = "70%";
+
+    const randomTransformX = getRndInteger(-500, 500);
+    const randomTransformY = getRndInteger(-500, 500);
+
+    cloud.style.transform = `translateX(${randomTransformX}px) translateY(${randomTransformY}px)`;
+    cloud.src = "assets/cloud.png";
+    sky.append(cloud);
+    cloud.classList.add("cloud");
+}
+
+function createCountryBanners(countryBanner) {
+    const countryIcon = document.createElement("img");
+
+    const randomSize = getRndInteger(25, 100);
+    countryIcon.style.width = randomSize + "px";
+    countryIcon.style.opacity = "60%";
+
+    const randomTransformX = getRndInteger(-1000, 1000);
+    const randomTransformY = getRndInteger(0, 0);
+
+    countryIcon.style.transform = `translateX(${randomTransformX}px) translateY(${randomTransformY}px)`;
+
+    if (countryBanner.parentElement.id == "japan") {
+        countryIcon.src = "assets/cherry-blossom-2.png";
+    } else if (countryBanner.parentElement.id == "france") {
+        countryIcon.src = "assets/croissant.png";
+    } else if (countryBanner.parentElement.id == "italy") {
+        countryIcon.src = "assets/tiramisu-emoji.webp";
+    }
+    
+    countryBanner.append(countryIcon);
+    countryIcon.classList.add("countryIcon");
+}
